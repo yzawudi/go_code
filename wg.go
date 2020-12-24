31b52039af89c95b56995d0e60dd5a0b79b24a11
yuzhouan <yuzhouan@qianxin.com>
2020-12-24 14:23:04 +0800

    wg.sunc

--- wg.go
+++ wg.go
@@ -12,3 +12,14 @@ func increment(wg *sync.WaitGroup, ch chan bool){
     <- ch
     wg.Done()
 }
+
+func main() {
+    var wg sync.WaitGroup
+    ch := make(chan bool, 1)
+    for i := 0; i < 1000; i++ {
+        wg.Add(1)
+        go increment(&wg, ch)
+    }
+    wg.Wait()
+    fmt.Println("final value of x",x)
+}
